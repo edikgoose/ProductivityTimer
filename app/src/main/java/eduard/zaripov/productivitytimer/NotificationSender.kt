f33b58a0c204c45521eca60c7ce54fd6d763ac07
edikgoose <edikgoose@gmail.com>
2022-04-11 00:32:00 +0300

    Forbid screen rotations

--- app/src/main/java/eduard/zaripov/productivitytimer/NotificationSender.kt
+++ app/src/main/java/eduard/zaripov/productivitytimer/NotificationSender.kt
@@ -1,12 +1,16 @@
 package eduard.zaripov.productivitytimer
 
+import android.R
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+
 
 class NotificationSender {
     companion object {
@@ -18,7 +22,7 @@ class NotificationSender {
 
             val channelId = "timeExceededChannel"
             val notificationBuilder = NotificationCompat.Builder(context, channelId)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setSmallIcon(R.drawable.ic_media_pause)
                 .setContentTitle(title)
                 .setContentText(message)
                 .setAutoCancel(true)
@@ -27,6 +31,17 @@ class NotificationSender {
 
             val notificationManager =
                 context.getSystemService(AppCompatActivity.NOTIFICATION_SERVICE) as NotificationManager
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(
+                    channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+                notificationManager.createNotificationChannel(channel)
+            }
+
+
             notificationManager.notify(reqCode, notificationBuilder.build())
         }
     }
